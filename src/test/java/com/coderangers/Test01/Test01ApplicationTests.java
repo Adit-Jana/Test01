3dbde91de93ab4d94b303446b228b1a708ac5174
Adit Jana <Adit.Jana@lntinfotech.com>
2023-04-26 14:27:33 +0530

    all files are adding

---
+++ src/test/java/com/coderangers/Test01/Test01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderangers.Test01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
